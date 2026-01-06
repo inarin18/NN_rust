4b0e5ec584ec7fdecec81549752bf2c1cf49f6bf
Hinata <hi710i7oka@outlook.jp>
2026-01-06 10:42:46 +0900

    Add OptimizerParams struct and update AbstractOptimizerTrait
    - Introduced the `OptimizerParams` struct with a constructor for better parameter management in optimizers.
    - Updated the `AbstractOptimizerTrait` to include a generic `Params` type and modified the `build` method to accept parameters, enhancing flexibility for future optimizer implementations.

--- src/optimizers/base_optimizer.rs
+++ src/optimizers/base_optimizer.rs
@@ -1,9 +1,20 @@
 use crate::model::Model;
+pub struct OptimizerParams {
+}
+
+impl OptimizerParams {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
 
 pub trait AbstractOptimizerTrait {
+    type Params;
+    
     fn update(&mut self, model: &mut Model);
     fn name(&self) -> &str;
-    fn build(&mut self);
+    fn build(&mut self, params: Self::Params);
+    
 }
 
 #[derive(Debug)]
@@ -11,12 +22,11 @@ pub struct AbstractOptimizer {
     pub name: String,
     pub learning_rate: Option<f32>,
     pub verbose: Option<bool>,
-    pub model: Option<Model>,
 }
 
 impl AbstractOptimizer {
     pub fn new(name: String) -> Self {
-        Self { name, learning_rate: None, verbose: None, model: None }
+        Self { name, learning_rate: None, verbose: None }
     }
 }
 
